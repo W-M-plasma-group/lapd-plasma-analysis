a25c788717fbc911a31015d3dbbb097c7fdb4cf3
LeoMurphyWM24 <ltmurphy@email.wm.edu>
2021-07-26 12:25:41 -0400

    Slight changes to main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ probe_area = (1. * u.mm) ** 2  # From MATLAB code
 core_region = 26. * u.cm
 ion_type = 'He-4+'
 hdf5_filename = 'HDF5/8-3500A.hdf5'
-# save_filename = 'netCDF/diagnostics.nc'
-save_filename = 'diagnostics.nc'
+# save_filename = 'netCDF/diagnostic_dataset.nc'
+save_filename = 'diagnostic_dataset.nc'
 open_filename = save_filename  # write to and read from the same location
 smoothing_margin = 10
 # End of global parameters
@@ -37,19 +37,20 @@ if not use_existing:
     bias, current = get_isweep_vsweep(hdf5_filename)  # get isweep and vsweep arrays
     # Put bias and current arrays in real units!
     characteristics = characterize_sweep_array(bias, current, margin=smoothing_margin, sample_sec=sample_sec)
-    # Analysis of single sample Isweep-Vsweep curve
-    """
-    sample_indices = (30, 0, 7)  # x position, y position, plateau number within frame
-    sample_plateau = characteristics[sample_indices]
-    pprint(swept_probe_analysis(sample_plateau, probe_area, ion_type, 
-                                visualize=True, plot_EEDF=True, bimaxwellian=True))
-    plt.show()
-    print("Done analyzing sample characteristic")
-    """
     diagnostics_dataset = plasma_diagnostics(characteristics, probe_area, ion_type, bimaxwellian=False)
     if save_diagnostics:
         write_netcdf(diagnostics_dataset, save_filename)
 
 radial_plot(diagnostics_dataset, diagnostic='T_e', plot='contour')
+
+# Analysis of single sample Isweep-Vsweep curve
+"""
+sample_indices = (30, 0, 7)  # x position, y position, plateau number within frame
+sample_plateau = characteristics[sample_indices]
+pprint(swept_probe_analysis(sample_plateau, probe_area, ion_type, 
+                            visualize=True, plot_EEDF=True, bimaxwellian=True))
+plt.show()
+print("Done analyzing sample characteristic")
+"""
 
 # Note: The non-bimaxwellian plasmapy electron temperature seems to be the *reciprocal* of the correct value.
