06981dc2cf904652dabe040b2dd21d75982f072d
LeoMurphyWM24 <ltmurphy@email.wm.edu>
2021-07-28 23:45:10 -0400

    Add time alignment code and start scaling factor calculation

--- interferometry.py
+++ interferometry.py
@@ -8,37 +8,91 @@ from hdf5reader import *
 
 def interferometry_calibration(density_xarray, temperature_xarray, interferometry_filename, bias, current,
                                steady_state_start_plateau, steady_state_end_plateau, core_region=26. * u.cm):
-    # Read in interferometry data
-    interferometry_file = open_hdf5(interferometry_filename)
-    interferometry_data_raw = item_at_path(interferometry_file,
-                                           '/MSI/Interferometer array/Interferometer [0]/Interferometer trace/')
-    interferometry_data_array = np.array(interferometry_data_raw)
+    # INTERFEROMETRY #
+    # ______________ #
 
-    print("Interferometry data shape:", interferometry_data_array.shape)
+    # Read in interferometry data ("interferometry" abbreviated as "inter" in variable names)
+    # TODO reduce all unneeded intermediate variable names to make clearer what variables *should* be used
+    # TODO keep all x and y data separate and optional, used only if corresponding dimension is not one element long
+    inter_file = open_hdf5(interferometry_filename)
+    inter_data_raw = item_at_path(inter_file, '/MSI/Interferometer array/Interferometer [0]/Interferometer trace/')
+    inter_data_array = np.array(inter_data_raw)
 
-    interferometry_means_abstract = np.mean(interferometry_data_array, axis=0)
-    interferometry_time_abstract = np.arange(len(interferometry_means_abstract))
+    print("Interferometry data shape:", inter_data_array.shape)
 
-    interferometry_data, interferometry_time = to_real_units_interferometry(interferometry_means_abstract,
-                                                                            interferometry_time_abstract)
-    # debug
-    # print(interferometry_data, "\n", interferometry_time)
-    #
+    inter_means_abstract = np.mean(inter_data_array, axis=0)
+    inter_time_abstract = np.arange(len(inter_means_abstract))
+
+    # Create interferometry DataArray with interferometry time as coordinates
+    inter_values, inter_time = to_real_units_interferometry(inter_means_abstract, inter_time_abstract)
+    inter_data = xr.DataArray(inter_values, coords=(('time', inter_time, {'units': str(u.ms)}),))
+
+    # print(inter_data)
+
+    # DENSITY #
+    # _______ #
 
     # Calculate density line integrals
     x_length, y_length, plateaus = density_xarray.sizes['x'], density_xarray.sizes['y'], density_xarray.sizes['plateau']
-
     density_xarray_cm = density_xarray * (1 / u.m ** 3).to(1 / u.cm ** 3).value
-    core_density_array = density_xarray_cm.where(xr.ufuncs.logical_and(
-        abs(density_xarray_cm.x) < core_region.value, abs(density_xarray_cm.y) < core_region.value), drop=True)
+
+    # Select elements of density array with both x and y in the core position range (|x| < 26 cm & |y| < 26 cm)
+    x_mask, y_mask = abs(density_xarray_cm.x) < core_region.value, abs(density_xarray_cm.y) < core_region.value
+    both = xr.ufuncs.logical_and  # Rename function for readability
+    core_density_array = density_xarray_cm.where(both(x_mask, y_mask), drop=True)
 
     x_integral = core_density_array.integrate('x')
     y_integral = core_density_array.integrate('y')
 
-    # Find index of collapse
+    # debug
+    # print("x integral:", x_integral, "y integral:", y_integral, sep="\n")
+    #
+
+    # INTERFEROMETRY - DENSITY TIME ALIGNMENT #
+    # _______________________________________ #
+
+    # Find index of interferometry collapse; at point with most negative slope
+    inter_collapse_time = inter_data.differentiate('time').idxmin('time')
 
     # debug
-    xr.DataArray(interferometry_data, coords=(('time', interferometry_time, {'units': str(u.ms)}),)).plot()
+    # print("Interferometry collapse time:", inter_collapse_time.item())
+    #
+
+    # Find time of density collapses in x and y (average out multidimensional components)
+    density_collapse_times = {'x': x_integral.idxmax('plateau').mean(), 'y': y_integral.idxmax('plateau').mean()}
+
+    # debug
+    # print("Density collapse times:", density_collapse_times)
+    #
+
+    aligned_inter_times = {'x_time': ('time', inter_data.coords['time'] - inter_collapse_time + density_collapse_times['x']),
+                           'y_time': ('time', inter_data.coords['time'] - inter_collapse_time + density_collapse_times['y'])}
+
+    inter_data = inter_data.assign_coords(aligned_inter_times)
+
+    # Find average step in density (n_e) time coordinate by dividing total time elapsed by number of time measurements
+    density_time_coord = density_xarray_cm.coords['plateau']
+    dt = (density_time_coord[-1] - density_time_coord[0])/len(density_time_coord)  # time step for density time coord.
+
+    # SCALING FACTOR CALCULATION #
+    # __________________________ #
+
+    # Find average interferometry value at each time (for each x-adjusted time and each y-adjusted time)
+    # x_time_interferometry = for t in inter_data.coords['time']:
+    inter_avg_x_time = xr.DataArray([inter_data.where(both(inter_data.coords['x_time'] > t - dt/2,
+                                                           inter_data.coords['x_time'] < t + dt/2)
+                                                      ).mean() for t in density_xarray_cm.coords['plateau']],
+                                    dims=['plateau'],
+                                    coords={'plateau': density_xarray_cm.coords['plateau']})
+    # inter_avg_x_time = inter_avg_x_time.assign_coords({'plateau': density_xarray_cm.coords['plateau']})
+    inter_avg_y_time = [inter_data.where(both(inter_data.coords['y_time'] > t - dt/2,
+                                              inter_data.coords['y_time'] < t + dt/2)
+                                         ).mean() for t in density_xarray_cm.coords['plateau']]
+
+    # debug
+    print("\nInter x time data:", inter_avg_x_time, "\nInter y time data:", inter_avg_y_time, sep="\n")
+    inter_data.plot()
+    # inter_collapse_index.
     plt.show()
     #
 
