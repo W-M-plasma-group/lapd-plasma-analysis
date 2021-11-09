9d365b63f6bdea701d07f3ce8d86b5f1372f6799
Leo Murphy <ltmurphy@email.wm.edu>
2021-11-09 11:24:31 -0500

    Revert "Clean up comments in main.py file"
    This reverts commit 6745618196f9b6005815a9a4bcf02ec2e5e79c9a.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-“””
+"""
 The lapd-plasma-analysis repository was written by Leo Murphy based on code 
 written in MATLAB by Conor Perks (MIT) and using the PlasmaPy online code library.
-“””
+"""
 from getIVsweep import *
 from characterization import *
 from diagnostics import *
@@ -15,25 +15,25 @@ from setup import *
 sample_sec = (100 / 16 * 1e6) ** (-1) * u.s                   # Note that this is small. 10^6 is in the denominator
 probe_area = (1. * u.mm) ** 2                                 # From MATLAB code
 core_region = 26. * u.cm                                      # From MATLAB code
-ion_type = ‘He-4+’
+ion_type = 'He-4+'
 bimaxwellian = False
 smoothing_margin = 0
 steady_state_start_plateau, steady_state_end_plateau = 5, 11  # From MATLAB code
 # End of global parameters
 
 # User file path names
-hdf5_path = “/Users/REPLACE-THIS-PATH.hdf5”   # Link to the HDF5 files, available on the GitHub repository Releases page 
+hdf5_path = "/Users/REPLACE-THIS-PATH.hdf5"   # Link to the HDF5 files, available on the GitHub repository Releases page 
 interferometry_filename = hdf5_path           # interferometry data stored in same HDF5 file
-save_diagnostic_name = “diagnostic_dataset”  # Note: file is saved to a subfolder “netcdf” to be created if necessary
+save_diagnostic_name = "diagnostic_dataset"  # Note: file is saved to a subfolder “netcdf” to be created if necessary
 open_diagnostic_name = save_diagnostic_name   # write to and read from the same location
-netcdf_subfolder_name = “netcdf”              # subfolder to save and read netcdf files; set to “” to use current folder
+netcdf_subfolder_name = "netcdf"              # subfolder to save and read netcdf files; set to “” to use current folder
 # End of file path names
 
 # User file options
-“”” Set the below variable to True to open an existing diagnostic dataset from a NetCDF file
-    or False to create a new diagnostic dataset from the given HDF5 file. “””
+""" Set the below variable to True to open an existing diagnostic dataset from a NetCDF file
+    or False to create a new diagnostic dataset from the given HDF5 file. """
 use_existing = True
-“”” Set the below variable to True to save diagnostic data to a NetCDF file if a new one is created from HDF5 data. “””
+""" Set the below variable to True to save diagnostic data to a NetCDF file if a new one is created from HDF5 data. """
 save_diagnostics = True
 # End of file options
 
@@ -45,7 +45,7 @@ open_diagnostic_path = netcdf_path(open_diagnostic_name, netcdf_subfolder_path, 
 
 # Read experimental parameters
 experimental_parameters, experimental_parameters_rounded = setup_lapd(hdf5_path)
-print(“Experimental parameters:”, {key: str(value) for key, value in experimental_parameters_rounded.items()})
+print("Experimental parameters:", {key: str(value) for key, value in experimental_parameters_rounded.items()})
 bias, current, x, y = get_isweep_vsweep(hdf5_path)
 
 # Create the diagnostics dataset by generating new or opening from a NetCDF file
@@ -56,46 +56,46 @@ if not diagnostics_dataset:  # diagnostic dataset not loaded; create new from HD
     if save_diagnostics:
         write_netcdf(diagnostics_dataset, save_diagnostic_path)
 
-print(“Plasma diagnostics:”, [key for key in diagnostics_dataset.keys()])
+print("Plasma diagnostics:", [key for key in diagnostics_dataset.keys()])
 
 # Contour plot of electron temperature
-radial_diagnostic_plot(diagnostics_dataset, diagnostic=‘T_e’, plot=‘contour’)
+radial_diagnostic_plot(diagnostics_dataset, diagnostic='T_e', plot='contour')
 
 # Uncomment this section to perform analysis of a single sample Isweep-Vsweep curve
-“””
+"""
 sample_indices = (30, 0, 7)  # x position, y position, plateau number within frame
 sample_plateau = characteristics[sample_indices].item()
 print(swept_probe_analysis(sample_plateau, probe_area, ion_type,
                            visualize=True, plot_EEDF=True, bimaxwellian=bimaxwellian))
 plt.show()
-“””
-“””
+"""
+"""
 print({diagnostic: diagnostics_dataset[diagnostic][sample_indices].values for diagnostic in diagnostics_dataset.keys()})
-print(“Done analyzing sample characteristic”)
-“””
+print("Done analyzing sample characteristic")
+"""
 
 # Note: in the future, this will consider only the steady-state region
 if not bimaxwellian:
-    pressure = (3 / 2) * diagnostics_dataset[‘n_e’] * diagnostics_dataset[‘T_e’] * (1. * u.eV * u.m ** -3).to(u.Pa)
+    pressure = (3 / 2) * diagnostics_dataset['n_e'] * diagnostics_dataset['T_e'] * (1. * u.eV * u.m ** -3).to(u.Pa)
     both = xr.ufuncs.logical_and
-    plateau = pressure.coords[‘plateau’]  # rename variables for comprehensibility
+    plateau = pressure.coords['plateau']  # rename variables for comprehensibility
 
     steady_state_pressure = pressure.where(both(plateau >= steady_state_start_plateau, plateau <= steady_state_end_plateau))
-    steady_state_pressure.squeeze(dim=‘y’).assign_attrs({“units”: str(u.Pa)}).plot.contourf(x=‘time’, y=‘x’, robust=True)
-    plt.title(“Steady state pressure”)
+    steady_state_pressure.squeeze(dim='y').assign_attrs({"units": str(u.Pa)}).plot.contourf(x='time', y='x', robust=True)
+    plt.title("Steady state pressure")
     plt.show()
 else:
-    print(“Pressure plotting not yet supported for bimaxwellian temperature distributions.”)
+    print("Pressure plotting not yet supported for bimaxwellian temperature distributions.")
 
 electron_density, density_scaling = interferometry_calibration(
-    diagnostics_dataset[‘n_e’], interferometry_filename,
+    diagnostics_dataset['n_e'], interferometry_filename,
     steady_state_start_plateau, steady_state_end_plateau, core_region=core_region)
 
 # Plot electron density data that was calibrated using interferometry data
 electron_density.squeeze().plot.contourf(robust=True)
 plt.show()
 
-neutral_ratio(diagnostics_dataset[‘n_e’], experimental_parameters, steady_state_start_plateau, steady_state_end_plateau)
+neutral_ratio(diagnostics_dataset['n_e'], experimental_parameters, steady_state_start_plateau, steady_state_end_plateau)
 
 # Note: Plot generation code and neutral fraction analysis is incomplete.
 # Note: The non-bimaxwellian electron temperature calculated using the PlasmaPy code seems to be 
